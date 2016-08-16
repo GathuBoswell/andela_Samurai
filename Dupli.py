d1e876f3632d58347a3deb75e4c337f6310c69bc
GathuBoswell <gathudev@gmail.com>
2016-08-16 10:20:51 +0300

    initial commit Dupli.py checks for consecutive duplicate numbers n a list

---
+++ Dupli.py
@@ -0,0 +1,17 @@
+def dupli(listy):
+    if len(listy) == 0:
+        return "List is Empty"
+    else:
+        temp = [listy[0]]
+        for i in range(len(listy) - 1):
+            if  temp[-1] == listy[i]:
+                continue
+            else:
+                temp.append(listy[i])
+        return temp
+A = [1,1,2,3,3,3,3,34,4,5,6,5,7,7]
+print(dupli(A))
+
+            
+        
+        
